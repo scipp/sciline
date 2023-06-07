c9b66599aa81762bebd6edd483d34704f0e8d04d
Simon Heybrock <simon.heybrock@ess.eu>
2023-06-07 15:06:03 +0200

    Update from template

---
+++ tests/test_package.py
@@ -0,0 +1,7 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (c) 2023 Scipp contributors (https://github.com/scipp)
+import sciline as pkg
+
+
+def test_has_version():
+    assert hasattr(pkg, '__version__')
