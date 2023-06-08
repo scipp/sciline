a4473346b3efaee7feed42aa919a3416ef0ce9b3
Simon Heybrock <simon.heybrock@ess.eu>
2023-06-08 12:57:49 +0200

    Add 'lazy' argument

--- src/sciline/container.py
+++ src/sciline/container.py
@@ -1,6 +1,7 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # Copyright (c) 2023 Scipp contributors (https://github.com/scipp)
 import typing
+from functools import wraps
 from typing import Callable, List, Type, TypeVar
 
 import injector
@@ -23,20 +24,47 @@ class Container:
             raise UnsatisfiedRequirement(e) from e
 
 
-def _injectable(func: Callable) -> Callable:
+def _delayed(func: Callable) -> Callable:
+    """
+    Decorator to make a function return a delayed object.
+
+    In contrast to dask.delayed, this uses functools.wraps, to preserve the
+    type hints, which is a prerequisite for injector to work.
+    """
+    import dask
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        return dask.delayed(func)(*args, **kwargs)
+
+    return wrapper
+
+
+def _injectable(func: Callable, *, lazy: bool) -> Callable:
     """
     Wrap a regular function so it can be registered in an injector and have its
     parameters injected.
     """
     tps = typing.get_type_hints(func)
+    # When lazy, we want to create a dask task graph without duplicate computation.
+    # This means we need to use a singleton scope, so that the function is only
+    # called once.
+    # When not lazy, we have to avoid memory consumption from injector holding on
+    # to large intermediate results. We therefore do not use a singleton scope.
+    # This is however problematic as multiple functions may rely on a result
+    # from a previous function. Maybe we need more manual control? Or maybe we can
+    # always use dask, but compute() automatically when not lazy?
+    scope = injector.singleton if lazy else None
+    if lazy:
+        func = _delayed(func)
 
     def bind(binder: injector.Binder):
-        binder.bind(tps['return'], injector.inject(func))
+        binder.bind(tps['return'], injector.inject(func), scope=scope)
 
     return bind
 
 
-def make_container(funcs: List[Callable], /) -> Container:
+def make_container(funcs: List[Callable], /, *, lazy: bool = False) -> Container:
     """
     Create a :py:class:`Container` from a list of functions.
 
@@ -44,9 +72,14 @@ def make_container(funcs: List[Callable], /) -> Container:
     ----------
     funcs:
         List of functions to be injected. Must be annotated with type hints.
+    lazy:
+        If True, the functions are wrapped in :py:func:`dask.delayed` before
+        being injected. This allows to build a dask graph from the container.
     """
     # Note that we disable auto_bind, to ensure we do not accidentally bind to
     # some default values. Everything must be explicit.
     return Container(
-        injector.Injector([_injectable(func) for func in funcs], auto_bind=False)
+        injector.Injector(
+            [_injectable(func, lazy=lazy) for func in funcs], auto_bind=False
+        )
     )
--- tests/container_test.py
+++ tests/container_test.py
@@ -1,26 +1,52 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # Copyright (c) 2023 Scipp contributors (https://github.com/scipp)
+import dask
 import pytest
 
 import sciline as sl
 
 
+def f(x: int) -> float:
+    return 0.5 * x
+
+
+def g() -> int:
+    return 3
+
+
+def h(x: int, y: float) -> str:
+    return f"{x} {y}"
+
+
 def test_make_container_sets_up_working_container():
-    def f(x: int) -> float:
-        return 0.5 * x
-
-    def g() -> int:
-        return 3
-
     container = sl.make_container([f, g])
     assert container.get(float) == 1.5
     assert container.get(int) == 3
 
 
 def test_make_container_does_not_autobind():
-    def f(x: int) -> float:
-        return 0.5 * x
-
     container = sl.make_container([f])
     with pytest.raises(sl.UnsatisfiedRequirement):
         container.get(float)
+
+
+def test_make_container_lazy_returns_task_that_computes_result():
+    container = sl.make_container([f, g], lazy=True)
+    task = container.get(float)
+    assert hasattr(task, 'compute')
+    assert task.compute() == 1.5
+
+
+def test_lazy_with_multiple_outputs_computes_intermediates_once():
+    ncall = 0
+
+    def provide_int() -> int:
+        nonlocal ncall
+        ncall += 1
+        return 3
+
+    container = sl.make_container([f, provide_int, h], lazy=True)
+    task1 = container.get(float)
+    task2 = container.get(str)
+    assert dask.compute(task1, task2) == (1.5, '3 1.5')
+    assert ncall == 1
