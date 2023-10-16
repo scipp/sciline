b263fffa489712471cee397015d4df6e204667f5
jokasimr <johanneskasimir@gmail.com>
2023-10-16 13:19:17 +0200

    fix: make names in the graph selectable in jupyter (#64)
    * feat: patch digraph repr to add text/html mimetype
    
    * fix: ignore missing type annotations

--- src/sciline/visualize.py
+++ src/sciline/visualize.py
@@ -15,10 +15,24 @@ from typing import (
     get_origin,
 )
 
-from graphviz import Digraph
+import graphviz
 
 from .pipeline import Pipeline, SeriesProvider
 from .typing import Graph, Item, Key, get_optional
+
+
+class Digraph(graphviz.Digraph):  # type: ignore[misc]
+    '''Replace the default `_repr_mimebundle_` implementation from graphviz.Digraph.
+    The default implementation does not return a 'text/html' mimetype.
+    The 'image/svg+xml' mimetype renders as an <img> node in Jupyter.
+    We want graphs to render a <svg> node to enable text selection in the graph.
+    '''
+
+    def _repr_mimebundle_(self, *args, **kwargs):  # type: ignore[no-untyped-def]
+        data = super()._repr_mimebundle_(*args, **kwargs)
+        if 'image/svg+xml' in data:
+            data['text/html'] = data['image/svg+xml']
+        return data
 
 
 @dataclass
