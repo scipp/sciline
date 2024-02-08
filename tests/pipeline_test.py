96def3f35058029c546665c25b0c12ea84ebc148
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2024-02-08 09:26:49 +0100

    Suppress type checks

--- tests/pipeline_test.py
+++ tests/pipeline_test.py
@@ -1253,15 +1253,15 @@ def test_pipeline_generic_keyword_only(scheduler: sl.scheduler.Scheduler) -> Non
 
 
 def test_pipeline_detect_missing_argument_typehint() -> None:
-    def f(x) -> int:
-        return x
+    def f(x) -> int:  # type: ignore[no-untyped-def]
+        return x  # type:ignore[no-any-return]
 
     with pytest.raises(ValueError, match='type-hint'):
         sl.Pipeline([f])
 
 
 def test_pipeline_detect_missing_return_typehint() -> None:
-    def f(x: int):
+    def f(x: int):  # type: ignore[no-untyped-def]
         return x
 
     with pytest.raises(ValueError, match='type-hint'):
