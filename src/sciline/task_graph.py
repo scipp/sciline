45a224a27dd82229932b13d4d71d1c3cdda242f6
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2024-02-12 14:38:25 +0100

    Support all provider kinds

--- src/sciline/task_graph.py
+++ src/sciline/task_graph.py
@@ -3,7 +3,7 @@
 from __future__ import annotations
 
 from html import escape
-from typing import Any, Generator, Optional, Sequence, Tuple, TypeVar, Union
+from typing import Any, Generator, Optional, Sequence, Tuple, TypeVar, Union, get_args
 
 from ._provider import Provider
 from ._utils import key_full_qualname, key_name, provider_full_qualname, provider_name
@@ -114,6 +114,7 @@ class TaskGraph:
         else:
             return self._scheduler.get(self._graph, [keys])[0]
 
+    # TODO remove again?
     def nodes(self) -> Generator[Union[Key, Provider], None, None]:
         """Iterate over all nodes of the graph.
 
@@ -175,7 +176,7 @@ class TaskGraph:
             nodes.append(_serialize_provider_node(provider, key, provider_id))
 
             edges.append(_serialize_edge(provider_id, key_id))
-            if provider.kind == 'function':
+            if provider.kind in ('function', 'series'):
                 for arg in provider.arg_spec.keys():
                     edges.append(_serialize_edge(node_ids.get(arg), provider_id))
 
@@ -224,6 +225,15 @@ class TaskGraph:
 
 
 def _serialize_data_node(key: Key, key_id: str) -> dict[str, str]:
+    if isinstance(key, Item):
+        return {
+            'id': key_id,
+            'kind': 'data_table_cell',
+            'label': key_name(key),
+            'value_type': key_full_qualname(key.tp),
+            'row_types': [key_full_qualname(label.tp) for label in key.label],
+            'row_indices': [key_full_qualname(label.index) for label in key.label],
+        }
     return {
         'id': key_id,
         'kind': 'data',
@@ -235,19 +245,38 @@ def _serialize_data_node(key: Key, key_id: str) -> dict[str, str]:
 def _serialize_provider_node(
     provider: Provider, key: Key, provider_id: str
 ) -> dict[str, str]:
+    from .pipeline import SeriesProvider
+
+    if isinstance(provider, SeriesProvider):
+        row_dim = provider.row_dim
+        value_type = get_args(key)[1]
+        return {
+            'id': provider_id,
+            'kind': 'p_series',
+            'label': f'provide_series[{key_name(row_dim)}, {key_name(value_type)}]',
+            'value_type': key_full_qualname(value_type),
+            'row_dim': key_full_qualname(row_dim),
+            'labels': list(map(key_full_qualname, provider.labels)),
+        }
     if provider.kind == 'function':
         return {
             'id': provider_id,
-            'kind': 'function',
+            'kind': 'p_function',
             'label': provider_name(provider),
             'function': provider_full_qualname(provider),
         }
     if provider.kind == 'parameter':
         return {
             'id': provider_id,
-            'kind': 'parameter',
+            'kind': 'p_parameter',
             'label': key_name(key),
             'type': key_full_qualname(key),
+        }
+    if provider.kind == 'table_cell':
+        return {
+            'id': provider_id,
+            'kind': 'p_table_cell',
+            'label': f'table_cell({key_name(key)})',
         }
     raise ValueError(
         f'Cannot serialize graph containing providers of kind {provider.kind}'
@@ -263,7 +292,7 @@ class _UniqueNodeId:
         self._assigned: dict[int, str] = {}
         self._next = 0
 
-    def get(self, obj: Any) -> str:
+    def get(self, obj: Union[Key, Provider]) -> str:
         hsh = hash(obj)
         try:
             return self._assigned[hsh]
